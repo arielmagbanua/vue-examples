c34c5dc63f3ece474bb49b2bbe8613a021bbb098
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-06-23 21:09:18 +0800

    event modifiers.

--- events/app.js
+++ events/app.js
@@ -2,9 +2,21 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
+    confirmName() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      event.preventDefault();
+      console.log('submitted');
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
     add(num) {
       this.counter = this.counter + num;
     },
